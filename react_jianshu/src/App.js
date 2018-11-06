ecbf044c1c9e98254f2966840a3f3464c5f4fe44
linqiqi077 <865530219@qq.com>
2018-11-06 20:36:51 +0800

    完成简书的公共头部

---
+++ react_jianshu/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from './common/header/index.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Header></Header>
+        hell world!
+      </div>
+    );
+  }
+}
+
+export default App;
